ca4ebc608a835076af2c163843a5b5ee14b7a773
dennisicecap1 <dennis.icecap1@gmail.com>
2022-12-03 01:49:27 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
+          Lab10c
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div id="paragraph"><p>
+        <h2>Setting Up</h2>
+        I installed ReactJS by first installing Node.js on my computer. I then ran the command <code>npm i -g npx</code> which installs npx.
+        <br></br><br></br>
+        After that I opened a folder, and in terminal typed the following commands: <code>npx create-react-app my-app</code>, <code>cd my-app</code> and <code>npm start</code>.
+        This let me create a new React app.
+        <br></br><br></br>
+        From there, I modified the App.js and App.css files using simple HTML and CSS. I kept the spinning React logo that came with the app by default.
+        <br></br><br></br>
+        Finally I uploaded the React app to Github Pages to host the site.
+      </p></div>
+      <div id="paragraph"><p>
+        <h2>Difficulties</h2>
+        At first, I did not realize that React apps were created using commands instead of simply creating a new file. I had to look at React tutorials to find out
+        how to create an app.
+        
+      </p></div>
     </div>
   );
 }
